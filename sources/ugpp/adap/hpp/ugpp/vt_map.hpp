6412a59e77257696a0ed3557d327ba93aa4c0a85
fasxmut <fasxmut>
2026-09-26 05:00:19 +0000

    Update ugpp::vt_map, ugpp::lt_map

--- sources/ugpp/adap/hpp/ugpp/vt_map.hpp
+++ sources/ugpp/adap/hpp/ugpp/vt_map.hpp
@@ -23,7 +23,7 @@ namespace ugpp
 				std::remove_cvref_t<t_key_type>,
 				t_key_type
 			>
-	using vt_map =
+	using vt_map_basis =	// basis is not base
 		ugpp::basic_vector_tuple_map<
 			std::allocator<
 				std::tuple<
@@ -37,5 +37,34 @@ namespace ugpp
 			t_other_value_type_list ...
 		>
 	;
+}	// namespace ugpp
+
+namespace ugpp
+{
+	template <
+		typename t_key_type,	// Requires a key-type and at least one vlaue-type
+		typename t_value1_type,
+		typename ... t_other_value_type_list
+	>
+		requires
+			std::same_as<
+				std::remove_cvref_t<t_key_type>,
+				t_key_type
+			>
+	class vt_map:
+		virtual public
+			ugpp::vt_map_basis<
+				t_key_type,
+				t_value1_type,
+				t_other_value_type_list ...
+			>
+	{
+	private:
+		using base_type = ugpp::vt_map_basis<t_key_type, t_value1_type, t_other_value_type_list ...>;
+		using self_type = ugpp::vt_map_basis<t_key_type, t_value1_type, t_other_value_type_list ...>;
+	public:
+		// basic_tuple_map is the real constructor.
+		using base_type::basic_tuple_map;
+	};
 }	// namespace ugpp
 
